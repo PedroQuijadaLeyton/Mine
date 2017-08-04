63ffc12552b734b32da91b1fb759842e5b0c802a
PedroQuijadaLeyton <pedro@quiley.com>
2017-08-04 20:11:43 +0000

    init unity

---
+++ Assets/Script/LevelController.cs
@@ -0,0 +1,204 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour {
+
+    public int level_rows;
+    public int level_columns;
+    public int[,] tile_data;
+    public TileController[,] tile_data_tilecontroller;
+    string tile_level;
+    int tilecontroller_index = 0;
+    Transform tiles_container;
+
+    public Sprite coin;
+    public Sprite bomb;
+    public Sprite diamond;
+    public Sprite marked;
+
+    public Text coin_text;
+    int coins_counter;
+    public Text diamond_text;
+    int diamond_counter;
+    public Text turns_text;
+    int tiles_left;
+    int tiles_left_go;
+
+    public int win_coins;
+    public int win_diamond;
+    public Text win_coins_text;
+    public Text win_diamond_text;
+    
+    public GameObject game_over;
+    public GameObject game_over_extra;
+    public GameObject win;
+    public GameObject win_extra;
+
+    int number_of_coins_available;
+    int number_of_diamonds_available;
+
+    int number_of_GO = 0;
+    int number_of_win = 0;
+
+    public int lives;
+
+    void Awake()
+    {
+        tile_data = new int[level_rows, level_columns];
+        tile_data_tilecontroller = new TileController[level_rows, level_columns];
+
+        tiles_container = GameObject.FindGameObjectWithTag("tiles").transform;
+
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                tile_data_tilecontroller[i, j] = tiles_container.GetChild(tilecontroller_index).GetComponent<TileController>();
+                tilecontroller_index++;
+            }
+        }
+
+        win_coins_text.text = "" + win_coins;
+        win_diamond_text.text = "" + win_diamond;
+        turns_text.text = "" + lives;
+    }
+
+    void Start()
+    {
+        generate_data();
+        pop_start();
+        //count_turns();
+    }
+
+    public void generate_data()
+    {
+        number_of_coins_available = number_of_diamonds_available = 0;
+        tile_level = "";
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                tile_data[i, j] = Random.Range(0, 4);
+                tile_level = tile_level + "" + tile_data[i, j];
+
+                tile_data_tilecontroller[i, j].generate_tile_data(tile_data[i, j], i, j);
+
+                if (tile_data[i, j] == 2)
+                    number_of_coins_available++;
+                if (tile_data[i, j] == 3)
+                    number_of_diamonds_available++;
+            }
+            tile_level = tile_level + "\n";
+        }
+        Debug.Log(tile_level);
+    }
+
+    void pop_start()
+    {
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                if (tile_data_tilecontroller[i, j].tile_value == 0)
+                {
+                    tile_data_tilecontroller[i, j].pop_when_0();
+                    break;
+                }
+            }
+        }
+    }
+
+    void restart()
+    {
+        coins_counter = diamond_counter = 0;
+        diamond_text.text = coin_text.text = "0";
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                tile_data_tilecontroller[i, j].restart();
+            }
+        }
+        game_over.SetActive(false);
+        game_over_extra.SetActive(false);
+        win.SetActive(false);
+        win_extra.SetActive(false);
+    }
+
+    public void generate_data_button()
+    {
+        restart();
+        generate_data();
+        pop_start();
+        //count_turns();
+    }
+
+    public void level_(int type_of_tile)
+    {
+        if(type_of_tile == 2)
+        {
+            coins_counter++;
+            number_of_coins_available--;
+            coin_text.text = "" + coins_counter;
+        }
+        else if(type_of_tile == 3)
+        {
+            diamond_counter++;
+            number_of_diamonds_available--;
+            diamond_text.text = "" + diamond_counter;
+        }
+        else if (type_of_tile == 1)
+        {
+            coins_counter--;
+            coin_text.text = "" + coins_counter;
+        }
+
+        Debug.Log(tiles_left_go);
+        if (coins_counter >= win_coins && diamond_counter >= win_diamond)
+            win.SetActive(true);
+        else if (tiles_left_go+1 == level_rows * level_columns)
+        {
+            lives--;
+            turns_text.text = "" + lives;
+            if(lives == 0)
+                game_over_extra.SetActive(true);
+            else
+                game_over.SetActive(true);
+        }
+
+        //turns_counter--;
+        //turns_text.text = "" + turns_counter;
+    }
+
+    void count_tiles_left_win()
+    {
+        tiles_left = 0;
+
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                if (tile_data_tilecontroller[i, j].is_active)
+                    tiles_left++;
+            }
+        }
+    }
+
+    public void count_tiles_left()
+    {
+        tiles_left_go = 0;
+
+        for (int i = 0; i < level_rows; i++)
+        {
+            for (int j = 0; j < level_columns; j++)
+            {
+                if (!tile_data_tilecontroller[i, j].is_active)
+                    tiles_left_go++;
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Script/TileController.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TileController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler{
+    
+    int row_number = 0;
+    int column_number = 0;
+    int number_of_1;
+    int number_of_2;
+    int number_of_3;
+    public int tile_value;
+    LevelController lc;
+    public bool is_active = true;
+    bool is_being_hold_down = false;
+    float timer_hold_down = 0;
+    float timer_hold_down_total = 0;
+    bool is_marked = false;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        lc = FindObjectOfType<LevelController>();
+    }
+
+    public void generate_tile_data(int tile_value_, int row_number_, int column_number_)
+    {
+        //asign value to the tile
+        // tile_value = lc.tile_data[row_number, column_number];
+        row_number = row_number_;
+        column_number = column_number_;
+        tile_value = tile_value_;
+        //transform.GetChild(0).GetComponent<Text>().text = "" + tile_value_;
+
+        number_of_1 = 0;
+        number_of_2 = 0;
+        number_of_3 = 0;
+
+        //read the neighbours
+        for (int i = row_number - 1; i <= row_number + 1; i++)
+        {
+            for (int j = column_number - 1; j <= column_number + 1; j++)
+            {
+                if (i >= 0 && i < lc.tile_data.GetLength(0) && j >= 0 && j < lc.tile_data.GetLength(0) && !(row_number == i && column_number == j))
+                    if (lc.tile_data[i, j] == 1)
+                        number_of_1++;
+                    else if(lc.tile_data[i, j] == 2)
+                        number_of_2++;
+            }
+        }
+    }
+
+    void on_click()
+    {
+        if(is_active)
+        {
+            if (tile_value != 0)
+            {
+
+                //asign value to the tile
+                tile_value = lc.tile_data[row_number, column_number];
+
+                //traps
+                number_of_1 = 0;
+                //coins
+                number_of_2 = 0;
+                //diamon
+                number_of_3 = 0;
+
+                //read the neighbours
+                for (int i = row_number - 1; i <= row_number + 1; i++)
+                {
+                    for (int j = column_number - 1; j <= column_number + 1; j++)
+                    {
+                        if (i >= 0 && i < lc.tile_data.GetLength(0) && j >= 0 && j < lc.tile_data.GetLength(0) && !(row_number == i && column_number == j))
+                            if (lc.tile_data[i, j] == 1)
+                                number_of_1++;
+                            else if (lc.tile_data[i, j] == 2)
+                                number_of_2++;
+                            else if (lc.tile_data[i, j] == 3)
+                                number_of_3++;
+                    }
+                }
+                //asign the text/data for the player
+                transform.GetChild(1).GetComponent<Text>().text = "" + number_of_1;
+                transform.GetChild(2).GetComponent<Text>().text = "" + number_of_2;
+                transform.GetChild(3).GetComponent<Text>().text = "" + number_of_3;
+            }
+            else
+            {
+                pop_when_0();
+            }
+        }
+        
+        
+    }
+
+    public void pop_when_0()
+    {
+        hide_tile();
+        //asign value to the tile
+        tile_value = lc.tile_data[row_number, column_number];
+
+        //traps
+        number_of_1 = 0;
+        //coins
+        number_of_2 = 0;
+        //diamons
+        number_of_3 = 0;
+
+        //read the neighbours
+        for (int i = row_number - 1; i <= row_number + 1; i++)
+        {
+            for (int j = column_number - 1; j <= column_number + 1; j++)
+            {
+                if (i >= 0 && i < lc.tile_data.GetLength(0) && j >= 0 && j < lc.tile_data.GetLength(0) && !(row_number == i && column_number == j))
+                    if (lc.tile_data[i, j] == 1)
+                        number_of_1++;
+                    else if (lc.tile_data[i, j] == 2)
+                        number_of_2++;
+                    else if (lc.tile_data[i, j] == 3)
+                        number_of_3++;
+            }
+        }
+        //asign the text/data for the player
+        transform.GetChild(1).GetComponent<Text>().text = "" + number_of_1;
+        transform.GetChild(2).GetComponent<Text>().text = "" + number_of_2;
+        transform.GetChild(3).GetComponent<Text>().text = "" + number_of_3;
+
+        for (int i = row_number - 1; i <= row_number + 1; i++)
+        {
+            for (int j = column_number - 1; j <= column_number + 1; j++)
+            {
+                if (i >= 0 && i < lc.tile_data.GetLength(0) && j >= 0 && j < lc.tile_data.GetLength(0) && lc.tile_data_tilecontroller[i, j].is_active)
+                {
+                    if (i == row_number || j == column_number)
+                        lc.tile_data_tilecontroller[i, j].on_click();
+
+                }
+
+            }
+        }
+    }
+
+    void hide_tile()
+    {
+        is_active = false;
+        gameObject.SetActive(false);
+    }
+
+    public void restart()
+    {
+        is_active = true;
+        gameObject.SetActive(true);
+        transform.GetChild(1).GetComponent<Text>().text = "";
+        transform.GetChild(2).GetComponent<Text>().text = "";
+        transform.GetChild(3).GetComponent<Text>().text = "";
+        GetComponent<Image>().sprite = null;
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        is_being_hold_down = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+
+        if (!is_marked && timer_hold_down_total < 1.5f)
+        {
+            if (tile_value == 1)
+                GetComponent<Image>().sprite = lc.bomb;
+            else if (tile_value == 2)
+                GetComponent<Image>().sprite = lc.coin;
+            else if (tile_value == 3)
+                GetComponent<Image>().sprite = lc.diamond;
+
+            if (is_active)
+            {
+                lc.level_(tile_value);
+                on_click();
+                is_active = false;
+            }
+        }
+
+        timer_hold_down_total = 0;
+        is_being_hold_down = false;
+        timer_hold_down = 0;
+        lc.count_tiles_left();
+    }
+
+    void Update()
+    {
+        if (is_being_hold_down)
+        {
+            timer_hold_down += Time.deltaTime;
+            timer_hold_down_total += Time.deltaTime;
+        }
+
+        if (timer_hold_down > 2.0f)
+        {
+            if(!is_marked)
+            {
+                is_marked = true;
+                GetComponent<Image>().sprite = lc.marked;
+            }
+            else
+            {
+                is_marked = false;
+                GetComponent<Image>().sprite = null;
+            }
+            timer_hold_down = 0;
+        }
+    }
+}
